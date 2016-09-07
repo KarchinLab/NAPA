6bb12b2b0fc327f4d664e7567b1a83d9d63142a6
Michael Ryan <mryan@insilico.us.com>
2016-09-07 10:57:46 -0400

    Files for generation of pip package.

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 
 setup(name='napa',
-      version='1.0',
+      version='0.8',
       description='Network Analysis of Protein Adaptation (intra-protein residue coevolution network construction and analysis)',
       url='http://karchinlab.org',
       author='Violeta Beleva-Guthrie',
       author_email='vbeleva@gmail.com',
       license='',
       packages=['napa'],
+      install_requires=[
+          'numpy',
+          'scipy',
+          'networkx',
+          'ete2'
+      ],
       zip_safe=False)
