1c56f8acda30986786c8a625806915388a096dba
vbguthrie <vbeleva@gmail.com>
2016-09-21 20:20:59 -0400

    Fixed dir names. Comments on optional/required inputs.
    Fixed directory structure to reflect the new analyze/ subdirectory. In the example, the optional vs. required arguments are indicated in the comments.

--- napa/tests/scripts/aln_net.run.sh
+++ napa/tests/scripts/aln_net.run.sh
@@ -1,23 +1,31 @@
 projDir=$1
-projDir=$projDir/NAPA
+projDir=$projDir/napa
 dataDir=$projDir/tests/data
 resultsDir=$projDir/tests/results
-scriptsDir=$projDir/tests/scripts
+analyzeDir=$projDir/analyze
 
-#Input file
-alnFASTA=$dataDir/classA.protein.fasta
-#fastaIDs=$dataDir/TEM-clin-ids.txt
-posList=$dataDir/TEM-pos2aa.txt
+#------------Input files------------------
+# Required: Input MSA in FASTA format
+alnFASTA=$dataDir/classA.protein.fasta 
+
+# Optional: Sequential numbering of positions
+posList=$dataDir/TEM-pos2aa.txt  
+
+# Optional: Subset of positions
 posSubset=$dataDir/TEM-posSubset.txt
+
+# Optional: Functional assignment for each sequence
 protFunc=$dataDir/TEM-functions.txt
+
+# Optional: List of selected functions assoc. with sequences
 selProtFunc=$dataDir/sel-funcs.txt
 
-#Output file: the network in tab-delimited format
+# Required output file path: the network in tab-delimited format
 netFile=$resultsDir/TEM-aln-net.txt
 
 #Alignment network construction and analysis
 #Jaccard index weighted undirected pairs
-python $scriptsDir/aln_mut_pairs.py \
+python $analyzeDir/aln_mut_pairs.py \
     -a  $alnFASTA \
     -p  $posList \
     -wt "TEM_1" \
